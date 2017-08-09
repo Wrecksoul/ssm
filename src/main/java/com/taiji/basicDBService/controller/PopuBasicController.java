fb5f8e6e5a77e578b58616cb99288cd475422ba8
mengxz <82427129@qq.com>
2017-08-09 16:19:32 +0800

    suiyi

--- src/main/java/com/taiji/basicDBService/controller/PopuBasicController.java
+++ src/main/java/com/taiji/basicDBService/controller/PopuBasicController.java
@@ -1,5 +1,19 @@
 package com.taiji.basicDBService.controller;
 
-public class PopuBasicController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class PopuBasicController {
+	Logger log = LoggerFactory.getLogger(PopuBasicController.class);
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String getStart(){
+		log.debug("finished");
+		return "finish";
+	}
 }
