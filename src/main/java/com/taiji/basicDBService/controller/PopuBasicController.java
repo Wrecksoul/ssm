4faa146369f53409cc2317e8596aeb2788dad92c
mengxz <82427129@qq.com>
2017-08-11 17:40:22 +0800

    log

--- src/main/java/com/taiji/basicDBService/controller/PopuBasicController.java
+++ src/main/java/com/taiji/basicDBService/controller/PopuBasicController.java
@@ -1,5 +1,9 @@
 package com.taiji.basicDBService.controller;
 
+import org.apache.logging.log4j.EventLogger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.message.StructuredDataMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,11 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PopuBasicController {
 	Logger log = LoggerFactory.getLogger(PopuBasicController.class);
+	private org.apache.logging.log4j.Logger logger = LogManager.getLogger(PopuBasicController.class);
 	
 	@RequestMapping("/")
 	@ResponseBody
 	public String getStart(){
-		log.debug("finished");
+		logger.debug("in menthod");
+		EventLogger.logEvent(new StructuredDataMessage("121", null, "type"));
+		logger.exit();
 		return "finish";
 	}
 }
